2f6f1bfc2dd1e6482e639aab9482a9d4449b71d8
Jenson Bolton <69874214+Jenson-Bolton@users.noreply.github.com>
2022-04-20 23:13:49 +0100

    changed ignore

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,34 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://cinvrhnlxmkszbrirgxg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjIxNzMzMCwiZXhwIjoxOTUxNzkzMzMwfQ.AUCfgsxVNn7LyUIoqqxYsF4Rqs-CeQEkLNBwKBzkpBo')
+
+const { user, session, error } = await supabase.auth.signIn({
+
+});
+
 describe('Supabase', () => {
 
-    it.todo('');
+    it('Get Notes', async () => {
 
-    it.todo('');
+        const { data, error } = await supabase
+        .from('cities')
+        .select();
 
-    it.todo('');
+        console.log(data);
+
+        expect(error).toBe(null);
+    
+    });
+
+    it.todo('Get Tasks');
+
+    it.todo('Get Bugs');
+
+    it.todo('Add Notes');
+
+    it.todo('Add Tasks');
+
+    it.todo('Add Bugs');
 
 });
